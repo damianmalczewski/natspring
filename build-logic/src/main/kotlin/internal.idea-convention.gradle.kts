8f6229bb19aad97951ef0154d36a86296c5d6e22
Damian Malczewski <damian.m.malczewski@gmail.com>
2026-06-13 19:16:21 +0200

    Add idea configurations for examples

--- build-logic/src/main/kotlin/internal.idea-convention.gradle.kts
+++ build-logic/src/main/kotlin/internal.idea-convention.gradle.kts
@@ -1,6 +1,7 @@
 import org.jetbrains.gradle.ext.Gradle
 import org.jetbrains.gradle.ext.runConfigurations
 import org.jetbrains.gradle.ext.settings
+import org.jetbrains.gradle.ext.Application
 
 plugins {
     id("org.jetbrains.gradle.plugin.idea-ext")
@@ -21,6 +22,32 @@ idea {
                 create<Gradle>("Gradle Format Code [natspring]") {
                     taskNames = listOf("spotlessApply")
                     projectPath = rootProject.rootDir.absolutePath
+                }
+
+                // pre-configured run configurations for example applications
+                create<Application>("Run [example-event-driven-telemetry]") {
+                    moduleName = "natspring.examples.example-event-driven-telemetry.main"
+                    mainClass = "org.example.natspring.telemetry.TelemetryApplication"
+                }
+                create<Application>("Run [example-jetstream]") {
+                    moduleName = "natspring.examples.example-jetstream.main"
+                    mainClass = "org.example.natspring.jetstream.JetStreamListenerExample"
+                }
+                create<Application>("Run [example-jetstream-deadletter]") {
+                    moduleName = "natspring.examples.example-jetstream-deadletter.main"
+                    mainClass = "org.example.natspring.jetstreamdeadletter.JetStreamDeadLetterExample"
+                }
+                create<Application>("Run [example-listener]") {
+                    moduleName = "natspring.examples.example-listener.main"
+                    mainClass = "org.example.natspring.listener.NatsListenerExample"
+                }
+                create<Application>("Run [example-listener-deadletter]") {
+                    moduleName = "natspring.examples.example-listener-deadletter.main"
+                    mainClass = "org.example.natspring.listenerdeadletter.ListenerDeadLetterExample"
+                }
+                create<Application>("Run [example-request]") {
+                    moduleName = "natspring.examples.example-request.main"
+                    mainClass = "org.example.natspring.request.RequestReplyExample"
                 }
             }
         }
